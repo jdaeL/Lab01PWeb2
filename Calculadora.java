3ffe090d501e4f0c1774532e8175896c74ad8afa
JDaelfc <jfloreschoque@unsa.edu.pe>
2023-05-04 21:26:30 -0500

    estructura

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 public class Calculadora
 {
     public static void main(String args[]) {
         // Calculadora
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Que operacion realizara?? ('+', '-', '*', '/')"); 
+        String operacion=sc.next();
+
+        System.out.println("Ingrese 2 operandos");
+        System.out.println("operando 1: ");
+        int x =sc.nextInt();
+        System.out.println("operand2 2: ");
+        int y =sc.nextInt();
+
 
     }
+
+    // insertar métodos
+
+    
 }
