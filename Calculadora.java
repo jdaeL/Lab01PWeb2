a46d7217189c7a4f61eb19472301b144253d9a54
CieloMV <cmezav@unsa.edu.pe>
2023-05-04 22:22:52 -0500

    Método suma

--- Calculadora.java
+++ Calculadora.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
-public class Calculadora
-{
+
+public class Calculadora {
     public static void main(String args[]) {
         // Calculadora
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Que operacion realizara?? ('+', '-', '*', '/')"); 
-        String operacion=sc.next();
+        System.out.println("Que operacion realizara?? ('+', '-', '*', '/')");
+        String operacion = sc.next();
 
         System.out.println("Ingrese 2 operandos");
         System.out.println("operando 1: ");
-        int x =sc.nextInt();
+        int x = sc.nextInt();
         System.out.println("operand2 2: ");
-        int y =sc.nextInt();
-
+        int y = sc.nextInt();
 
     }
 
-    // insertar métodos
+    public int suma(int x, int y) {
+        return x + y;
+    }
 
-    
 }
